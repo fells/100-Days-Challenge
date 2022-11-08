cf1ac0c96523d17977f5b7c238d71975e1b7f60f
Michel Calil <micheldcalil@hotmail.com>
2022-11-08 14:40:49 -0300

    Started Day 6

---
+++ Day6/day6.py
@@ -0,0 +1,43 @@
+"""
+        CODE BLOCKS
+
+        FUNCTIONS
+
+        def FunctionName (variable(s) that will be used in order for the function to work OR it can be null):
+            Do this
+
+        FunctionName() --> Calling the function to execute its CodeBlock
+
+        Example:
+
+        def PrintName(name):
+            print(f"Hello {name}")
+
+        PrintName("Fabio")
+
+        INDENTATION
+
+        def MyFunction():
+        ....print("Hello")  --> The dots are the indentation, meaning the print function is inside th MyFunction FUNCTION
+        MyFunction()  --> This is outside of the CodeBlock of the FUNCTION
+
+        WHILE LOOPS
+
+
+        While something_is_true  --> Do something while is true, else END the Loop
+            Do something repeatedly
+
+
+        Example:
+
+        num = 6
+
+        while num > 0:
+            num -= 1
+            print(num)
+"""
+
+# Final Project
+
+
+# All done in the website (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json)
