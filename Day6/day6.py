83d6ff50e9a1f97ea3ca98d82f32eeb18455eead
Michel Calil <micheldcalil@hotmail.com>
2022-11-08 16:58:56 -0300

    Finished Day 6

--- Day6/day6.py
+++ Day6/day6.py
@@ -40,4 +40,4 @@
 # Final Project
 
 
-# All done in the website (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json)
+# All done in the website (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
