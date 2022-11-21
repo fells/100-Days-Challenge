8d1ee836a8109129ebd194878b0f1fe6183c4755
Michel Calil <micheldcalil@hotmail.com>
2022-11-20 21:23:54 -0300

    Finished Day 15

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,7 +1,7 @@
 # Final Project
 # Create a Coffee Machine
 
-coins = 0
+profit = 0
 COIN_VALUE = [0.01, 0.05, 0.10, 0.25]
 FLAVORS = {
     "espresso": {
@@ -35,59 +35,72 @@ resources = {
     "coffee": 100
 }
 
-report = str(f"""
-    REPORT
-    Water: {resources["water"]}ml
-    Milk: {resources["milk"]}ml
-    Coffee: {resources["coffee"]}g
-    Money: ${coins}
-""")
-
 
 def money_input(choice, price):
-    quarters = int(input("How many quarters ?: "))
-    dimes = int(input("How many dimes ?: "))
-    nickel = int(input("How many nickel ?: "))
-    penny = int(input("How many penny ?: "))
-    quarters = quarters * COIN_VALUE[3]
-    dimes = dimes * COIN_VALUE[2]
-    nickel = nickel * COIN_VALUE[1]
-    penny = penny * COIN_VALUE[0]
+    quarters = int(input("How many quarters ?: ")) * COIN_VALUE[3]
+    dimes = int(input("How many dimes ?: ")) * COIN_VALUE[2]
+    nickel = int(input("How many nickel ?: ")) * COIN_VALUE[1]
+    penny = int(input("How many penny ?: ")) * COIN_VALUE[0]
     total = quarters + dimes + nickel + penny
-    change = total - price
-
-    if total >= price:
-        print(f"Here is your change: {'%.2f' % change}")
-        print(f"Enjoy you're {choice}. ☕")
-    else:
-        print(f"Not enough money")
-        money_input(choice, price)
+    return total
 
 
 def is_resource_enough(order_ingredients):
+    """Return's True when the order can be made, and False when it can't"""
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item}")
             return False
     return True
 
+
+def is_transaction_successful(payment, price, user_choice):
+    """Check if the amount of money that the user inserted is at least the necessary amount"""
+    change = payment - price
+    if payment >= price:
+        global profit
+        profit += price
+        print(f"Here is your change: {'%.2f' % change}")
+        print(f"Enjoy you're {user_choice}. ☕")
+        return True
+    else:
+        print(f"Sorry, not enough money")
+        return False
+
+
+def make_coffee(user_choice, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"")
+
 def coffee_machine():
     make_more_coffee = False
     while not make_more_coffee:
         user_choice = input("What would you like ? (Espresso/Latte/Cappuccino):\n").lower()
         if user_choice == "report":
-            print(report)
-        else:
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${profit}")
+        elif user_choice in FLAVORS:
             drink = FLAVORS[user_choice]
             if is_resource_enough(drink["ingredients"]):
                 price = drink["price"]
                 print("Insert Coin")
-                money_input(user_choice, price)
-                more_coffee = input("Do you want more coffee ? 'Y' or 'N'\n")
-                if more_coffee == "y" or more_coffee == "yes":
-                    coffee_machine()
-                else:
-                    print("See ya later. 👋")
-                    make_more_coffee = True
+                payment = money_input(user_choice, price)
+                success = is_transaction_successful(payment, price, user_choice)
+
+
+
+            more_coffee = input("Do you want more coffee ? 'Y' or 'N'\n")
+            if more_coffee == "y" or more_coffee == "yes":
+                coffee_machine()
+            else:
+                print("See ya later. 👋")
+                make_more_coffee = True
+        else:
+            print("Please input a correct option.")
+            coffee_machine()
 
 coffee_machine()
