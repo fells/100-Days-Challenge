ec0dcc4a38f7b1f99147add99608ef5601c92273
Michel Calil <micheldcalil@hotmail.com>
2022-11-21 17:30:01 -0300

    Finished Day 16

--- Day16/day16.py
+++ Day16/day16.py
@@ -49,3 +49,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like ? ({options})")
+    if choice == "off":
+        print("See ya. 👋")
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if money_machine.make_payment(drink.cost) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
