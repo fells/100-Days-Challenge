60914edfd5c70e7e424daea5fbe40d8a740ce827
Michel Calil <micheldcalil@hotmail.com>
2022-11-25 16:18:48 -0300

    Finished Day 20

--- Day20_21/day20_21.py
+++ Day20_21/day20_21.py
@@ -17,6 +17,12 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 snake = Snake()
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Right", fun=snake.right)
+
 game_is_on = True
 while game_is_on:
     screen.update()
--- Day20_21/snake.py
+++ Day20_21/snake.py
@@ -1,10 +1,16 @@
 import turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
         self.body = []
         self.create_snake()
+        self.head = self.body[0]
 
     def create_snake(self):
         for segment in STARTING_POSITIONS:
@@ -20,4 +26,20 @@ class Snake:
             new_x = self.body[segment - 1].xcor()
             new_y = self.body[segment - 1].ycor()
             self.body[segment].goto(x=new_x, y=new_y)
-        self.body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
