aa2dcfc12e0b1964ef25cf14a6ce05a27770214b
Michel Calil <micheldcalil@hotmail.com>
2022-12-12 20:47:28 -0300

    Finished Day 25

--- Day25/day25.py
+++ Day25/day25.py
@@ -76,5 +76,41 @@
             data.to_csv("new_data.csv")  --> Converting this DF into a new CSV file
 
 """
-
+# FINAL PROJECT
 import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("US States Game")
+screen.setup(width=725, height=491)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# Reading the states Files
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
+                                    prompt="What's another state's name").title()
+    if answer_state == "Exit":
+        # Generating a CSV file which will contain the remaining states that you haven`t guessed
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        data = pandas.DataFrame(missing_states)
+        data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# Generating a CSV file which will contain the remaining states that you haven`t guessed
