f3489ec1bffb5f0b90c9313a5e4fabfaef221cbc
Michel Calil <micheldcalil@hotmail.com>
2022-12-26 11:01:29 -0300

    Finished Day 29

--- Day29/day29.py
+++ Day29/day29.py
@@ -1,8 +1,10 @@
 # Final Project
 # Creating a Password generator with Tkinter
 from tkinter import *
+from tkinter import messagebox
 import string
 import random
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -12,7 +14,7 @@ def generate_password():
     numbers = list(string.digits)
     symbols = list(string.punctuation)
     all_characters = list(lower_alphabet + upper_alphabet + numbers + symbols)
-    random_num = random.randint(8, 20)
+    random_num = random.randint(8, 16)
     password_list = []
 
     for char in range(random_num):
@@ -20,22 +22,43 @@ def generate_password():
 
     password_string = "".join(password_list)
 
-    print(password_string)
+    entry_password.insert(0, password_string)
+    pyperclip.copy(password_string)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = entry_website.get()
+    email = entry_email_user.get()
+    password = entry_password.get()
+
+    if len(password) == 0 or len(website) == 0:
+        messagebox.showerror(title="Error", message="Please complete the form in order to save")
+
+    else:
+        is_ok = messagebox.askokcancel(title=f"{website}", message=f"These are the details entered: \n"
+                                                           f"Email: {email}\n"
+                                                           f"Password: {password}\n"
+                                                           f"Is it ok to save ?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                entry_website.delete(0, END)
+                entry_password.delete(0, END)
+
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Generator")
-window.config(padx=100, pady=100)
+window.config(padx=50, pady=50)
 
 # Logo
 photo = PhotoImage(file="logo.png")
-canvas = Canvas(width=200, height=189)
-canvas.create_image(100, 95, image=photo)
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=photo)
 canvas.grid(column=1, row=0)
 
 # Labels
@@ -43,30 +66,28 @@ label_website = Label(text="Website:")
 label_email_user = Label(text="Email/Username:")
 label_password = Label(text="Password:")
 
-label_website.config(padx=20)
-label_email_user.config(padx=20)
-label_password.config(padx=20)
-
 label_website.grid(column=0, row=1)
 label_email_user.grid(column=0, row=2)
 label_password.grid(column=0, row=3)
 
-# Entrys
+# Entry's
 
-entry_website = Entry(width=40)
-entry_email_user = Entry(width=40)
-entry_password = Entry(width=20)
+entry_website = Entry(width=35)
+entry_website.focus()
+entry_email_user = Entry(width=35)
+entry_email_user.insert(0, "micheldcalil@hotmail.com")
+entry_password = Entry(width=21)
 
-entry_website.grid(column=1, row=1)
-entry_email_user.grid(column=1, row=2)
-entry_password.grid(column=1, row=3, rowspan=2)
+entry_website.grid(column=1, row=1, columnspan=2)
+entry_email_user.grid(column=1, row=2, columnspan=2)
+entry_password.grid(column=1, row=3)
 
 # Buttons
 
-btn_generate_password = Button(text="Generate Password", width=20, command=generate_password)
-btn_add = Button(text="Add", width=80)
+btn_generate_password = Button(text="Generate Password", command=generate_password)
+btn_add = Button(text="Add", width=33, command=save)
 
-btn_generate_password.grid(column=1, row=3)
-btn_add.grid(column=1, row=4)
+btn_generate_password.grid(column=2, row=3)
+btn_add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
