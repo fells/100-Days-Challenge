fb573c9dad3e3a202923189cf9aa312a78a0db1f
Michel Calil <micheldcalil@hotmail.com>
2023-01-03 10:51:42 -0300

    Incremented the Password Generator

--- Day29/day29.py
+++ Day29/day29.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 import string
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -31,22 +32,59 @@ def save():
     website = entry_website.get()
     email = entry_email_user.get()
     password = entry_password.get()
+    new_data = {website: {
+        "email": email,
+        "password": password,
+    }}
 
     if len(password) == 0 or len(website) == 0:
         messagebox.showerror(title="Error", message="Please complete the form in order to save")
 
     else:
         is_ok = messagebox.askokcancel(title=f"{website}", message=f"These are the details entered: \n"
-                                                           f"Email: {email}\n"
-                                                           f"Password: {password}\n"
-                                                           f"Is it ok to save ?")
+                                                                   f"Email: {email}\n"
+                                                                   f"Password: {password}\n"
+                                                                   f"Is it ok to save ?")
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("data.json", "r") as data_file:  # --> Changing from a Text file to a Json file
+                    # Reading old data
+                    data = json.load(data_file)
+
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+
+            else:
+                # Updating old data with a new data
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+
+            finally:
                 entry_website.delete(0, END)
                 entry_password.delete(0, END)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = entry_website.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Website: {website}\n"
+                                                       f"Email: {email}\n"
+                                                       f"Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -72,13 +110,13 @@ label_password.grid(column=0, row=3)
 
 # Entry's
 
-entry_website = Entry(width=35)
+entry_website = Entry(width=21)
 entry_website.focus()
 entry_email_user = Entry(width=35)
 entry_email_user.insert(0, "micheldcalil@hotmail.com")
 entry_password = Entry(width=21)
 
-entry_website.grid(column=1, row=1, columnspan=2)
+entry_website.grid(column=1, row=1, columnspan=1)
 entry_email_user.grid(column=1, row=2, columnspan=2)
 entry_password.grid(column=1, row=3)
 
@@ -86,8 +124,10 @@ entry_password.grid(column=1, row=3)
 
 btn_generate_password = Button(text="Generate Password", command=generate_password)
 btn_add = Button(text="Add", width=33, command=save)
+btn_search = Button(text="Search", command=find_password)
 
 btn_generate_password.grid(column=2, row=3)
 btn_add.grid(column=1, row=4, columnspan=2)
+btn_search.grid(column=2, row=1)
 
 window.mainloop()
