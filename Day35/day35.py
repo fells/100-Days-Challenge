e4f2a870dc4c185e50ac6781e5019f15c64598b1
Michel Calil <micheldcalil@hotmail.com>
2023-01-17 22:27:13 -0300

    Finished Day 35

--- Day35/day35.py
+++ Day35/day35.py
@@ -6,11 +6,12 @@
 # Final Project
 import requests
 import json
+from twilio.rest import Client
 
 parameters = {
-    "lat": , # --> Personal Latitude
-    "lon": , # --> Personal Longitude
-    "appid": '', # --> Personal OpenWeather API Key
+    "lat": ,  # --> Personal Latitude
+    "lon": ,  # --> Personal Longitude
+    "appid": '',  # --> Personal OpenWeather API Key
     "units": 'metric',
     "exclude": "current,minutely,daily"
 }
@@ -22,6 +23,7 @@ data.raise_for_status()
 data_text = data.text
 data_json = json.loads(data_text)
 
+# Creating the Logic to send the message
 weather_slice = data_json["hourly"][:12]
 will_rain = False
 
@@ -30,6 +32,20 @@ for hour_data in weather_slice:
     if int(condition_code) < 700:
         will_rain = True
 
+
 if will_rain:
-    print("Bring an umbrella. ☂️")
+
+    message_to_send = "Bring an umbrella, it's going to rain. ☂️" # --> Message to send
+
+    # Making a connection with the Twilio API
+    twilio_sid_code = ''  # --> Personal SID code from Twilio
+    twilio_auth_code = ''  # Personal Authorization code from Twilio
+
+    client = Client(twilio_sid_code, twilio_auth_code)
+
+    message = client.messages.create(
+        body=message_to_send,  # --> Message that you want to send
+        from_="",  # --> Twilio's Phone number
+        to="")  # --> Personal Phone number
+
 
