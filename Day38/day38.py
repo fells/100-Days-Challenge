b64603de8cee86e2aa27c25bba01607f175f7bde
Michel Calil <micheldcalil@hotmail.com>
2023-01-23 16:37:45 -0300

    Finished Day 38

--- Day38/day38.py
+++ Day38/day38.py
@@ -4,19 +4,22 @@
 
 # Final Project
 # Creating a Workout Tracking program
+
 import requests
 import json
+from datetime import datetime
+import os
 
-APP_ID = ""  # --> Add your Nutrition API ID
-API_KEY = ""  # --> Add your Nutrition API Key
+APP_ID = os.environ.get("APP_ID")  # --> Add your Nutrition API ID
+API_KEY = os.environ.get("API_KEY")  # --> Add your Nutrition API Key
 
-GENDER = "" # --> Add your Gender
-WEIGHT_KG = ""  # --> Add your Weight
-HEIGHT_CM = ""  # --> Add your Height
-AGE = "" # --> Add your Age
+GENDER = "male"  # --> Add your Gender
+WEIGHT_KG = "86"  # --> Add your Weight
+HEIGHT_CM = "180"  # --> Add your Height
+AGE = "26"  # --> Add your Age
 
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheets_endpoint = "" # --> Add your Sheety API endpoint
+sheets_endpoint = os.environ.get("sheets_endpoint")  # --> Add your Sheety API endpoint
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -38,12 +41,25 @@ parameters = {
 nutrion_data = requests.request("POST", endpoint, headers=headers, json=parameters)
 nutrion_data.raise_for_status()
 nutrition_json = json.loads(nutrion_data.text)
-exercise_duration = nutrition_json["exercises"][0]["duration_min"]
-exercise_name = nutrition_json["exercises"][0]["name"]
-print(f"{exercise_name}\n{exercise_duration}")
+
+# Creating the date and adding in to the Sheety API
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
 
 # Creating a connection with the Sheety API
 
-# sheets_data = requests.get(sheets_endpoint)
-# sheets_data.raise_for_status()
-# print(sheets_data.text)
+for exercise in nutrition_json["exercises"]:
+    sheets_input = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+    sheets_data = requests.post(sheets_endpoint, json=sheets_input, auth=("", ""))  # --> Add in your username and password
+    sheets_data.raise_for_status()
+
+    print(sheets_data.text)
